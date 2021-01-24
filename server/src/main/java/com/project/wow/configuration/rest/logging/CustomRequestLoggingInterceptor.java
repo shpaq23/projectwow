ef504213d2cffccecd5872a66869d3482f74debc
Przemek Banasik <p.banasik@storware.eu>
2021-01-24 22:54:23 +0100

    modified rest logging

--- server/src/main/java/com/project/wow/configuration/rest/logging/CustomRequestLoggingInterceptor.java
+++ server/src/main/java/com/project/wow/configuration/rest/logging/CustomRequestLoggingInterceptor.java
@@ -2,6 +2,7 @@ package com.project.wow.configuration.rest.logging;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -14,13 +15,13 @@ public class CustomRequestLoggingInterceptor extends HandlerInterceptorAdapter {
     private static final Logger log = LoggerFactory.getLogger(CustomRequestLoggingInterceptor.class);
 
     @Override
-    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) {
         log.info("REQUEST: method=[{}], path = [{}]", request.getMethod(), request.getRequestURI());
         return true;
     }
 
     @Override
     public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
-        log.info("RESPONSE Status: [{}]", response.getStatus());
+        log.info("RESPONSE: status= [{}] code = [{}]", HttpStatus.resolve(response.getStatus()).name(), response.getStatus());
     }
 }
