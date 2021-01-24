b7196a63b73be25cd2c6480e0d894fe605bfae9a
Przemek Banasik <p.banasik@storware.eu>
2021-01-24 22:46:32 +0100

    modified rest logging

---
+++ server/src/main/java/com/project/wow/configuration/rest/logging/CustomRequestLoggingInterceptor.java
@@ -0,0 +1,26 @@
+package com.project.wow.configuration.rest.logging;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component
+public class CustomRequestLoggingInterceptor extends HandlerInterceptorAdapter {
+
+    private static final Logger log = LoggerFactory.getLogger(CustomRequestLoggingInterceptor.class);
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler){
+        log.info("REQUEST: method=[{}], path = [{}]", request.getMethod(), request.getRequestURI());
+        return true;
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) {
+        log.info("RESPONSE Status: [{}]", response.getStatus());
+    }
+}
---
+++ server/src/main/java/com/project/wow/configuration/rest/logging/WebMvcConfiguration.java
@@ -0,0 +1,15 @@
+package com.project.wow.configuration.rest.logging;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(new CustomRequestLoggingInterceptor());
+    }
+
+}
